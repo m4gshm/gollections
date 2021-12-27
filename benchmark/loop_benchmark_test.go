9af51b5aa10010ae71652ef7b4e5b8cd632e675c
alekbulgakov <alekbulgakov@ozon.ru>
2021-12-27 14:39:01 +0300

    makes the MapIter reseteable

--- benchmark/loop_benchmark_test.go
+++ benchmark/loop_benchmark_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/m4gshm/container/iter"
+	implIter "github.com/m4gshm/container/iter/impl/iter"
 )
 
 var amount = 100_000
@@ -13,12 +14,13 @@ func Benchmark_ForEach_Iterator_Interface(b *testing.B) {
 	for i := 0; i < amount; i++ {
 		values[i] = i
 	}
+	result := 0
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make([]int, 0, 100000)
-		iter.ForEach(iter.Wrap(values), func(v int) { result = append(result, v) })
+		iter.ForEach(iter.Wrap(values), func(v int) { result = v })
 	}
 	b.StopTimer()
+	_ = result
 }
 
 func Benchmark_ForEach_Iterator_Impl(b *testing.B) {
@@ -26,91 +28,164 @@ func Benchmark_ForEach_Iterator_Impl(b *testing.B) {
 	for i := 0; i < amount; i++ {
 		values[i] = i
 	}
+	result := 0
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make([]int, 0, 100000)
-		iter.ForEach(iter.Wrap(values), func(v int) { result = append(result, v) })
+		implIter.ForEach(implIter.New(values), func(v int) { result = v })
 	}
 	b.StopTimer()
+	_ = result
 }
 
-func Benchmark_IterateHasNextGet_Iterator_Interface(b *testing.B) {
+func Benchmark_HasNextGet_Iterator_Interface(b *testing.B) {
 	values := make([]int, amount)
 	for i := 0; i < amount; i++ {
 		values[i] = i
 	}
+	result := 0
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make([]int, 0, 100000)
 		it := iter.Wrap(values)
 		for it.HasNext() {
-			result = append(result, it.Get())
+			result = it.Get()
 		}
 	}
 	b.StopTimer()
+	_ = result
 }
 
-func Benchmark_IterateHasNextGet_Iterator_Impl(b *testing.B) {
+func Benchmark_HasNextGet_Iterator_Impl(b *testing.B) {
 	values := make([]int, amount)
 	for i := 0; i < amount; i++ {
 		values[i] = i
 	}
+	result := 0
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make([]int, 0, 100000)
-		it := iter.Wrap(values)
+		it := implIter.New(values); 
 		for it.HasNext() {
-			result = append(result, it.Get())
+			result = it.Get()
 		}
 	}
 	b.StopTimer()
+	_ = result
 }
 
-func Benchmark_WrapMapHasNextGet(b *testing.B) {
+func Benchmark_HasNextGetReset_Iterator_Impl(b *testing.B) {
+	values := make([]int, amount)
+	for i := 0; i < amount; i++ {
+		values[i] = i
+	}
+	it := implIter.NewReseteable(values)
+	result := 0
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for it.HasNext() {
+			result = it.Get()
+		}
+		it.Reset()
+	}
+	b.StopTimer()
+	_ = result
+}
+
+func Benchmark_ForRange_EmbeddedSlice(b *testing.B) {
+	values := make([]int, amount)
+	for i := 0; i < amount; i++ {
+		values[i] = i
+	}
+	result := 0
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, v := range values {
+			result += v
+		}
+	}
+	b.StopTimer()
+	_ = result
+}
+
+func Benchmark_ForByIndex_EmbeddedSlice(b *testing.B) {
+	values := make([]int, amount)
+	for i := 0; i < amount; i++ {
+		values[i] = i
+	}
+	result := 0
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		for j := 0; j < len(values); j++ {
+			v := values[j]
+			result += v
+		}
+
+	}
+	b.StopTimer()
+	_ = result
+}
+
+func Benchmark_WrapMap_HasNextGet(b *testing.B) {
 	values := map[int]int{}
 	for i := 0; i < 100000; i++ {
 		values[i] = i
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make(map[int]int, len(values))
 		iter := iter.WrapMap(values)
 		for iter.HasNext() {
 			kv := iter.Get()
-			result[kv.Key()] = kv.Value()
+			_ = kv.Key()
+			_ = kv.Value()
 		}
 	}
 	b.StopTimer()
 }
 
-func Benchmark_NewMapHasNextGet(b *testing.B) {
+func Benchmark_NewMap_HasNextGet(b *testing.B) {
 	values := map[int]int{}
 	for i := 0; i < 100000; i++ {
 		values[i] = i
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make(map[int]int, len(values))
 		iter := iter.NewMap(values)
 		for iter.HasNext() {
 			kv := iter.Get()
-			result[kv.Key()] = kv.Value()
+			_ = kv.Key()
+			_ = kv.Value()
 		}
 	}
 	b.StopTimer()
 }
 
-func Benchmark_EmbeddedMapFor(b *testing.B) {
+func Benchmark_NewMap_HasNextGet_Reset(b *testing.B) {
+	values := map[int]int{}
+	for i := 0; i < 100000; i++ {
+		values[i] = i
+	}
+	iter := iter.NewMap(values)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for iter.HasNext() {
+			kv := iter.Get()
+			_ = kv.Key()
+			_ = kv.Value()
+		}
+		iter.Reset()
+	}
+	b.StopTimer()
+}
+
+func Benchmark_EmbeddedMap_For(b *testing.B) {
 	values := map[int]int{}
 	for i := 0; i < 100000; i++ {
 		values[i] = i
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		result := make(map[int]int, len(values))
-
 		for k, v := range values {
-			result[k] = v
+			_ = k
+			_ = v
 		}
 	}
 	b.StopTimer()
--- iter/test/map_test.go
+++ iter/test/map_test.go
@@ -14,14 +14,41 @@ func Test_MapIterator(t *testing.T) {
 
 	values := map[string]*s{"first": {"first_name"}, "second": {"first_second"}}
 	result := map[string]*s{}
-	it := iter.WrapMap(values)
-	for it.HasNext() {
-		kv := it.Get()
-		result[kv.Key()] = kv.Value()
+
+	for it := iter.NewMap(values); it.HasNext(); {
+		k, v := it.Get().Get()
+		result[k] = v
 	}
 	assert.Equal(t, len(values), len(values))
 
 	for k, v := range values {
 		assert.Equal(t, v, result[k])
+	}
+}
+
+func Test_MapIterateResetIterat(t *testing.T) {
+	type s struct {
+		name string
+	}
+
+	values := map[string]*s{"first": {"first_name"}, "second": {"first_second"}}
+	result1 := map[string]*s{}
+
+	it := iter.NewMap(values)
+	for it.HasNext() {
+		k, v := it.Get().Get()
+		result1[k] = v
+	}
+	it.Reset()
+
+	result2 := map[string]*s{}
+	for it.HasNext() {
+		k, v := it.Get().Get()
+		result2[k] = v
+	}
+
+	for k, v := range values {
+		assert.Equal(t, v, result1[k])
+		assert.Equal(t, v, result2[k])
 	}
 }
