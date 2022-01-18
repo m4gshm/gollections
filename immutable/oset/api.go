9f0f8a35aaced75f6c27497d7ec9936b98c4d09f
alekbulgakov <alekbulgakov@ozon.ru>
2022-01-18 18:51:28 +0300

    moves the ordered set impl to oset packages

--- immutable/oset/api.go
+++ immutable/oset/api.go
@@ -1,4 +1,4 @@
-package set
+package oset
 
 import (
 	"github.com/m4gshm/gollections/immutable"
@@ -6,6 +6,10 @@ import (
 
 func Of[T comparable](elements ...T) immutable.Set[T] {
 	return ToOrderedSet(elements)
+}
+
+func Empty[T comparable]() immutable.Set[T] {
+	return NewOrderedSet[T](0)
 }
 
 func New[T comparable](capacity int) immutable.Set[T] {
