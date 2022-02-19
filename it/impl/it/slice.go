52d144b8aae23dec43279e2f35d7e0f9bd53588b
Bulgakov Alexander <buls@yandex.ru>
2022-02-20 01:26:15 +0300

    immutable.Vector's Iterator optimization

--- it/impl/it/slice.go
+++ it/impl/it/slice.go
@@ -11,15 +11,19 @@ import (
 const NoStarted = -1
 
 func NewHead[T any, TS ~[]T](elements TS) Iter[T] {
-	return NewHeadS[T](elements, GetTypeSize[T]())
+	return NewHeadS(elements, GetTypeSize[T]())
 }
 
 func NewHeadS[T any, TS ~[]T](elements TS, elementSize uintptr) Iter[T] {
-	header := GetSliceHeader[T](elements)
+	var (
+		header = GetSliceHeaderByRef(unsafe.Pointer(&elements))
+		size   = header.Len
+	)
 	return Iter[T]{
 		array:       unsafe.Pointer(header.Data),
 		elementSize: elementSize,
-		size:        header.Len,
+		size:        size,
+		maxHasNext:  size - 2,
 		current:     NoStarted,
 	}
 }
@@ -30,22 +34,23 @@ func NewTail[T any](elements []T) Iter[T] {
 
 func NewTailS[T any](elements []T, elementSize uintptr) Iter[T] {
 	var (
-		header = GetSliceHeader(elements)
+		header = GetSliceHeaderByRef(unsafe.Pointer(&elements))
 		size   = header.Len
 	)
 	return Iter[T]{
 		array:       unsafe.Pointer(header.Data),
 		elementSize: elementSize,
 		size:        size,
+		maxHasNext:  size - 2,
 		current:     size,
 	}
 }
 
 //Iter is the Iterator implementation.
 type Iter[T any] struct {
-	array         unsafe.Pointer
-	elementSize   uintptr
-	size, current int
+	array                     unsafe.Pointer
+	elementSize               uintptr
+	size, maxHasNext, current int
 }
 
 var (
@@ -54,11 +59,11 @@ var (
 )
 
 func (i *Iter[T]) HasNext() bool {
-	return HasNextBySize(i.size, i.current)
+	return CanIterateByRange(NoStarted, i.maxHasNext, i.current)
 }
 
 func (i *Iter[T]) HasPrev() bool {
-	return HasPrevBySize(i.size, i.current)
+	return CanIterateByRange(1, i.size, i.current)
 }
 
 func (i *Iter[T]) Next() T {
@@ -67,9 +72,11 @@ func (i *Iter[T]) Next() T {
 }
 
 func (i *Iter[T]) GetNext() (T, bool) {
-	if i.HasNext() {
-		i.current++
-		return GetArrayElem[T](i.array, i.current, i.elementSize), true
+	current := i.current
+	if CanIterateByRange(NoStarted, i.maxHasNext, current) {
+		current++
+		i.current = current
+		return *(*T)(GetArrayElemRef(i.array, current, i.elementSize)), true
 	}
 	var no T
 	return no, false
@@ -81,16 +88,23 @@ func (i *Iter[T]) Prev() T {
 }
 
 func (i *Iter[T]) GetPrev() (T, bool) {
-	if i.HasPrev() {
-		i.current--
-		return GetArrayElem[T](i.array, i.current, i.elementSize), true
+	current := i.current
+	if CanIterateByRange(1, i.size, current) {
+		current--
+		i.current = current
+		return *(*T)(GetArrayElemRef(i.array, current, i.elementSize)), true
 	}
 	var no T
 	return no, false
 }
 
-func (i *Iter[T]) Get() T {
-	return GetArrayElem[T](i.array, i.current, i.elementSize)
+func (i *Iter[T]) Get() (T, bool) {
+	current := i.current
+	if IsValidIndex(i.size, current) {
+		return *(*T)(GetArrayElemRef(i.array, current, i.elementSize)), true
+	}
+	var no T
+	return no, false
 }
 
 //HasNext checks the next element in an iterator by indexs of a current element and slice length.
@@ -105,17 +119,26 @@ func HasPrev[T any](elements []T, current int) bool {
 
 //HasNextBySize checks the next element in an iterator by indexs of a current element and slice length.
 func HasNextBySize(size int, current int) bool {
-	return HasNextByRange(NoStarted, size-2, current)
+	return CanIterateByRange(NoStarted, size-2, current)
 }
 
 //HasPrevBySize checks the previos element in an iterator by indexs of a current element and slice length.
 func HasPrevBySize(size, current int) bool {
-	return HasNextByRange(1, size, current)
+	return CanIterateByRange(1, size, current)
 }
 
-//HasNextByRange checks the next element in an iterator by indexes of the first, the last and a current elements of an underlying slice.
-func HasNextByRange(first, last, current int) bool {
+//CanIterateByRange checks the next element in an iterator by indexes of the first, the last and a current elements of an underlying slice.
+func CanIterateByRange(first, last, current int) bool {
 	return current >= first && current <= last
+}
+
+//IsValidIndex checks if index is out of range
+func IsValidIndex(size, index int) bool {
+	return index > -1 && index < size
+}
+
+func IsValidIndex2(size, index int) bool {
+	return !((index ^ size == 0) || index < 0)
 }
 
 //Get safely returns an element of a slice by an index or zero value of T if the index is out of range.
@@ -149,7 +172,11 @@ func GetTypeSize[T any]() uintptr {
 
 //GetArrayElem returns an element by index from the array referenced by an unsafe pointer
 func GetArrayElem[T any](array unsafe.Pointer, index int, elemSize uintptr) T {
-	return *(*T)(unsafe.Pointer(uintptr(array) + uintptr(index)*elemSize))
+	return *(*T)(GetArrayElemRef(array, index, elemSize))
+}
+
+func GetArrayElemRef(array unsafe.Pointer, index int, elemSize uintptr) unsafe.Pointer {
+	return unsafe.Pointer(uintptr(array) + uintptr(index)*elemSize)
 }
 
 //GetLen returns the length of elements
@@ -158,6 +185,10 @@ func GetLen[T any](elements []T) int {
 }
 
 //GetSliceHeader retrieves the SliceHeader of elements
-func GetSliceHeader[T any, TS ~[]T](elements TS) *reflect.SliceHeader {
-	return (*reflect.SliceHeader)(unsafe.Pointer(&elements))
+func GetSliceHeader[T any](elements []T) *reflect.SliceHeader {
+	return GetSliceHeaderByRef(unsafe.Pointer(&elements))
+}
+
+func GetSliceHeaderByRef(elements unsafe.Pointer) *reflect.SliceHeader {
+	return (*reflect.SliceHeader)(elements)
 }
--- it/impl/it/slice_test.go
+++ it/impl/it/slice_test.go
@@ -34,10 +34,220 @@ func Test_PointerBasedIter2(t *testing.T) {
 	expected := []someType{{"123", 123}, {"2", 2}, {"3", 3}, {"4", 4}}
 	iter := NewHead(expected)
 	result := make([]someType, 0)
-	for v, ok:= iter.GetNext(); ok; v,ok=iter.GetNext() {
+	for v, ok := iter.GetNext(); ok; v, ok = iter.GetNext() {
 		result = append(result, v)
 	}
 
 	assert.Equal(t, expected, result)
 }
 
+func Test_PointerBasedIter2Reverse(t *testing.T) {
+
+	type someType struct {
+		field1 string
+		field2 int64
+	}
+
+	values := []someType{{"123", 123}, {"2", 2}, {"3", 3}, {"4", 4}}
+	expected := []someType{{"4", 4}, {"3", 3}, {"2", 2}, {"123", 123}}
+	iter := NewTail(values)
+	result := make([]someType, 0)
+	for v, ok := iter.GetPrev(); ok; v, ok = iter.GetPrev() {
+		result = append(result, v)
+	}
+
+	assert.Equal(t, expected, result)
+}
+
+func Test_PointerBasedIterTailGetNext(t *testing.T) {
+
+	type someType struct {
+		field1 string
+		field2 int64
+	}
+
+	values := []someType{{"123", 123}, {"2", 2}, {"3", 3}, {"4", 4}}
+	iter := NewTail(values)
+
+	v, ok := iter.Get() //out of range
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext() //no more elements, because the iterator has not been started
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetPrev() //start from the latest element
+	assert.True(t, ok)
+	assert.Equal(t, someType{"4", 4}, v)
+
+	v, ok = iter.Get() //gets the current element
+	assert.True(t, ok)
+	assert.Equal(t, someType{"4", 4}, v)
+
+	v, ok = iter.GetNext()
+	_ = v
+	assert.False(t, ok) //no more elements after the latest
+
+	v, ok = iter.GetPrev() //gets prev
+	assert.Equal(t, someType{"3", 3}, v)
+	assert.True(t, ok)
+
+	v, ok = iter.Get() //gets the current element
+	assert.True(t, ok)
+	assert.Equal(t, someType{"3", 3}, v)
+
+	v, ok = iter.GetNext()
+	assert.True(t, ok) //returns to the latest
+	assert.Equal(t, someType{"4", 4}, v)
+
+	v, ok = iter.Get() //gets the current element
+	assert.True(t, ok)
+	assert.Equal(t, someType{"4", 4}, v)
+}
+
+func Test_PointerBasedIterHeadGetPrev(t *testing.T) {
+
+	type someType struct {
+		field1 string
+		field2 int64
+	}
+
+	values := []someType{{"123", 123}, {"2", 2}, {"3", 3}, {"4", 4}}
+	iter := NewHead(values)
+
+	v, ok := iter.GetPrev()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"123", 123}, v)
+
+	v, ok = iter.Get()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"123", 123}, v)
+
+	v, ok = iter.GetPrev()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"2", 2}, v)
+
+	v, ok = iter.Get()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"2", 2}, v)
+
+	v, ok = iter.GetPrev()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"123", 123}, v)
+
+	v, ok = iter.Get()
+	assert.True(t, ok)
+	assert.Equal(t, someType{"123", 123}, v)
+}
+
+func Test_PointerBasedEmptyIter(t *testing.T) {
+
+	type someType struct {
+		field1 string
+		field2 int64
+	}
+
+	values := []someType{}
+
+	iter := NewHead(values)
+
+	v, ok := iter.GetPrev()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext()
+	assert.False(t, ok)
+
+	v, ok = iter.Get()
+	assert.False(t, ok)
+
+	//tail
+
+	iter = NewTail(values)
+
+	v, ok = iter.GetPrev()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext()
+	assert.False(t, ok)
+
+	v, ok = iter.Get()
+	assert.False(t, ok)
+}
+
+func Test_PointerBasedOneElementIter(t *testing.T) {
+
+	type someType struct {
+		field1 string
+		field2 int64
+	}
+
+	values := []someType{{"only one", 1}}
+	iter := NewHead(values)
+
+	v, ok := iter.GetPrev()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetNext()
+	assert.True(t, ok)
+
+	v, ok = iter.Get()
+	assert.True(t, ok)
+
+	iter = NewTail(values)
+
+	v, ok = iter.GetNext()
+	_ = v
+	assert.False(t, ok)
+
+	v, ok = iter.GetPrev()
+	assert.True(t, ok)
+
+	v, ok = iter.Get()
+	assert.True(t, ok)
+}
+
+func Test_CanIterateByRange(t *testing.T) {
+	r := CanIterateByRange(NoStarted, 5, 4)
+	assert.True(t, r)
+
+	r = CanIterateByRange(NoStarted, 5, 6)
+	assert.False(t, r)
+
+	r = CanIterateByRange(NoStarted, 5, NoStarted)
+	assert.True(t, r)
+}
+
+func Test_IsValidIndex(t *testing.T) {
+	r := IsValidIndex(5, 0)
+	assert.True(t, r)
+
+	r = IsValidIndex(5, 5)
+	assert.False(t, r)
+
+	r = IsValidIndex(5, -1)
+	assert.False(t, r)
+
+}
+
+func Test_IsValidIndex2(t *testing.T) {
+	r := IsValidIndex2(5, 0)
+	assert.True(t, r)
+
+	r = IsValidIndex2(5, 5)
+	assert.False(t, r)
+
+	r = IsValidIndex2(5, -1)
+	assert.False(t, r)
+}
