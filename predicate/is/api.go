c757f1cab310b4ebba4ff7599261c741204efdc9
Bulgakov Alexander <buls@yandex.ru>
2024-06-16 01:27:03 +0300

    comments

--- predicate/is/api.go
+++ predicate/is/api.go
@@ -4,7 +4,7 @@ import (
 	"github.com/m4gshm/gollections/predicate"
 )
 
-// Not negates the 'p' predicate
+// Not - is.Not creates a 'not p' predicate.
 func Not[T any](p predicate.Predicate[T]) predicate.Predicate[T] {
 	return predicate.Not(p)
 }
