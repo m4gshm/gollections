2aaa545d728e517ea2ff7c46bb4c7600ef77f2cf
Alexander <mfourgeneralsherman@gmail.com>
2024-06-30 20:08:14 +0300

    feat(seq): api

--- seq/api.go
+++ seq/api.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/m4gshm/gollections/c"
 	"github.com/m4gshm/gollections/op"
-	"google.golang.org/genproto/googleapis/cloud/functions/v1"
 )
 
 // Of creates an iterator over the elements.
@@ -18,48 +17,6 @@ func Of[T any](elements ...T) iter.Seq[T] {
 			}
 		}
 	}
-}
-
-// Filter creates an iterator that iterates only those elements for which the 'filter' function returns true.
-func Filter[T any](seq iter.Seq[T], filter func(T) bool) iter.Seq[T] {
-	if seq == nil {
-		return nil
-	}
-	return func(consumer func(T) bool) {
-		seq(func(e T) bool {
-			if filter(e) {
-				return consumer(e)
-			}
-			return true
-		})
-	}
-}
-
-// Convert creates an iterator that applies the 'converter' function to each iterable element.
-func Convert[From, To any](seq iter.Seq[From], converter func(From) To) iter.Seq[To] {
-	if seq == nil {
-		return func(yield func(To) bool) {}
-	}
-	return func(consumer func(To) bool) {
-		seq(func(from From) bool {
-			return consumer(converter(from))
-		})
-	}
-}
-
-// Conv creates an iterator that applies the 'converter' function to each iterable element and returns value-error pairs.
-// The error should be checked at every iteration step, like:
-//
-//	var integers []int
-//	...
-//	for s, err := range seq.Conv(integers,  strconv.Itoa) {
-//	    if err != nil {
-//	        break
-//	    }
-//	    ...
-//	}
-func Conv[From, To any](seq iter.Seq[From], converter func(From) (To, error)) iter.Seq2[To, error] {
-	return ToSeq2(seq, converter)
 }
 
 // ToSeq2 converts an iterator of single elements to an iterator of key/value pairs by applying the 'converter' function to each iterable element.
@@ -74,9 +31,40 @@ func ToSeq2[T, K, V any](seq iter.Seq[T], converter func(T) (K, V)) iter.Seq2[K,
 	}
 }
 
-// KeyValue converts an iterator of single elements to a key/value pairs iterator by applying key, value extractors to each iterable element.
-func KeyValue[T, K, V any](seq iter.Seq[T], keyExtractor func(T) K, valExtractor func(T) V) iter.Seq2[K, V] {
-	return ToSeq2(seq, func(t T) (K, V) { return keyExtractor(t), valExtractor(t) })
+// First returns the first element that satisfies the condition of the 'predicate' function
+func First[T any](seq iter.Seq[T], predicate func(T) bool) (v T, ok bool) {
+	if seq == nil {
+		return
+	}
+	seq(func(one T) bool {
+		if predicate(one) {
+			v = one
+			ok = true
+			return false
+		}
+		return true
+	})
+	return
+}
+
+// Firstt returns the first element that satisfies the condition of the 'predicate' function
+func Firstt[T any](seq iter.Seq[T], predicate func(T) (bool, error)) (v T, ok bool, err error) {
+	if seq == nil {
+		return v, false, nil
+	}
+	seq(func(one T) bool {
+		p, e := predicate(one)
+		if e != nil {
+			err = e
+			return false
+		} else if p {
+			v = one
+			ok = true
+			return false
+		}
+		return true
+	})
+	return v, ok, err
 }
 
 // Slice collects the elements of the 'seq' sequence into a new slice
@@ -107,28 +95,56 @@ func Append[T any, TS ~[]T](seq iter.Seq[T], out TS) TS {
 	return out
 }
 
-
 func Reduce[T any](seq iter.Seq[T], merge func(T, T) T) T {
 	result, _ := ReduceOK(seq, merge)
 	return result
 }
 
 // ReduceOK reduces the elements retrieved by the 'seq' iterator into an one using the 'merge' function.
+// Returns ok==false if the seq returns ok=false at the first call (no more elements).
 func ReduceOK[T any](seq iter.Seq[T], merge func(T, T) T) (result T, ok bool) {
 	if seq == nil {
 		return result, false
 	}
-	first := true
+	started := false
 	seq(func(v T) bool {
-		if first {
+		if !started {
 			result = v
 		} else {
 			result = merge(result, v)
 		}
-		first = false
+		started = true
 		return true
 	})
-	return result, true
+	return result, started
+}
+
+// Reducee reduces the elements retrieved by the 'next' function into an one pair using the 'merge' function.
+func Reducee[T any](seq iter.Seq[T], merge func(T, T) (T, error)) (T, error) {
+	result, _, err := ReduceeOK(seq, merge)
+	return result, err
+}
+
+// ReduceeOK reduces the elements retrieved by the 'seq' iterator into an one using the 'merge' function.
+// Returns ok==false if the seq returns ok=false at the first call (no more elements).
+func ReduceeOK[T any](seq iter.Seq[T], merge func(T, T) (T, error)) (result T, ok bool, err error) {
+	if seq == nil {
+		return result, false, nil
+	}
+	started := false
+	seq(func(v T) bool {
+		if !started {
+			result = v
+		} else {
+			result, err = merge(result, v)
+			if err != nil {
+				return false
+			}
+		}
+		started = true
+		return true
+	})
+	return result, started, err
 }
 
 func Accum[T any](first T, seq iter.Seq[T], merge func(T, T) T) T {
@@ -137,32 +153,29 @@ func Accum[T any](first T, seq iter.Seq[T], merge func(T, T) T) T {
 		return accumulator
 	}
 
-	seq(func (v T) bool {
+	seq(func(v T) bool {
 		accumulator = merge(accumulator, v)
 		return true
 	})
 	return accumulator
 }
 
-// First returns the first element that satisfies the condition of the 'predicate' function
-func First[T any](seq iter.Seq[T], predicate func(T) bool) (v T, ok bool) {
+func Accumm[T any](first T, seq iter.Seq[T], merge func(T, T) (T, error)) (accumulator T, err error) {
+	accumulator = first
 	if seq == nil {
-		return
+		return accumulator, nil
 	}
-	seq(func(one T) bool {
-		if predicate(one) {
-			v = one
-			ok = true
-			return false
-		}
-		return true
+	seq(func(v T) bool {
+		accumulator, err = merge(accumulator, v)
+		return err == nil
 	})
-	return
+	return accumulator, err
+
 }
 
 // Sum returns the sum of all elements
-func Sum[T c.Summable](seq iter.Seq[T]) (T, bool) {
-	return ReduceOK(seq, op.Sum[T])
+func Sum[T c.Summable](seq iter.Seq[T]) (out T) {
+	return Accum(out, seq, op.Sum[T])
 }
 
 // HasAny finds the first element that satisfies the 'predicate' function condition and returns true if successful
@@ -182,4 +195,95 @@ func Contains[T comparable](seq iter.Seq[T], example T) bool {
 		return !contains
 	})
 	return contains
+}
+
+// Conv creates an iterator that applies the 'converter' function to each iterable element and returns value-error pairs.
+// The error should be checked at every iteration step, like:
+//
+//	var integers []int
+//	...
+//	for s, err := range seq.Conv(integers,  strconv.Itoa) {
+//	    if err != nil {
+//	        break
+//	    }
+//	    ...
+//	}
+func Conv[From, To any](seq iter.Seq[From], converter func(From) (To, error)) iter.Seq2[To, error] {
+	return ToSeq2(seq, converter)
+}
+
+// Convert creates an iterator that applies the 'converter' function to each iterable element.
+func Convert[From, To any](seq iter.Seq[From], converter func(From) To) iter.Seq[To] {
+	if seq == nil {
+		return func(yield func(To) bool) {}
+	}
+	return func(consumer func(To) bool) {
+		seq(func(from From) bool {
+			return consumer(converter(from))
+		})
+	}
+}
+
+// Flat is used to iterate ove multidimension sequences in single dimension form, like:
+//
+//	var arrays iter.Seq[[]int]
+//	...
+//	for e := range seq.Flat(arrays, as.Is) {
+//	    ...
+//	}
+func Flat[From, To any](seq iter.Seq[From], flattener func(From) []To) iter.Seq[To] {
+	if seq == nil {
+		return func(yield func(To) bool) {}
+	}
+	return func(yield func(To) bool) {
+		seq(func(v From) bool {
+			elementsTo := flattener(v)
+			for _, e := range elementsTo {
+				if !yield(e) {
+					return false
+				}
+			}
+			return true
+		})
+	}
+}
+
+// Filter creates an iterator that iterates only those elements for which the 'filter' function returns true.
+func Filter[T any](seq iter.Seq[T], filter func(T) bool) iter.Seq[T] {
+	if seq == nil {
+		return func(yield func(T) bool) {}
+	}
+	return func(consumer func(T) bool) {
+		seq(func(e T) bool {
+			if filter(e) {
+				return consumer(e)
+			}
+			return true
+		})
+	}
+}
+
+func Filt[T any](seq iter.Seq[T], filter func(T) (bool, error)) iter.Seq2[T, error] {
+	if seq == nil {
+		return func(yield func(T, error) bool) {}
+	}
+	var err error
+	return func(yield func(T, error) bool) {
+		seq(func(e T) bool {
+			if err != nil {
+				return yield(e, err)
+			}
+			ok := false
+			ok, err = filter(e)
+			if ok {
+				return yield(e, nil)
+			}
+			return true
+		})
+	}
+}
+
+// KeyValue converts an iterator of single elements to a key/value pairs iterator by applying key, value extractors to each iterable element.
+func KeyValue[T, K, V any](seq iter.Seq[T], keyExtractor func(T) K, valExtractor func(T) V) iter.Seq2[K, V] {
+	return ToSeq2(seq, func(t T) (K, V) { return keyExtractor(t), valExtractor(t) })
 }
--- seq/test/api_test.go
+++ seq/test/api_test.go
@@ -1,34 +1,117 @@
 package test
 
 import (
+	"errors"
+	"iter"
 	"testing"
 
 	"github.com/m4gshm/gollections/op"
 	"github.com/m4gshm/gollections/predicate/more"
 	"github.com/m4gshm/gollections/seq"
+	"github.com/m4gshm/gollections/seq2"
+	"github.com/m4gshm/gollections/slice"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ReduceSum(t *testing.T) {
+func Test_AccumSum(t *testing.T) {
+	s := seq.Of(1, 3, 5, 7, 9, 11)
+	r := seq.Accum(100, s, op.Sum[int])
+	assert.Equal(t, 100+1+3+5+7+9+11, r)
+}
 
+func Test_AccummSum(t *testing.T) {
+	s := seq.Of(1, 3, 5, 7, 9, 11)
+	r, err := seq.Accumm(100, s, func(i1, i2 int) (int, error) {
+		if i2 == 11 {
+			return i1, errors.New("stop")
+		}
+		return i1 + i2, nil
+	})
+	assert.Equal(t, 100+1+3+5+7+9, r)
+	assert.ErrorContains(t, err, "stop")
+}
+
+func Test_ReduceSum(t *testing.T) {
 	sum, ok := seq.ReduceOK(seq.Of(1, 2, 3, 4, 5, 6), op.Sum)
 
 	assert.True(t, ok)
 	assert.Equal(t, 21, sum)
 }
 
-func Test_ReduceContains(t *testing.T) {
+func Test_ReduceeSum(t *testing.T) {
+	s := seq.Of(1, 3, 5, 7, 9, 11)
+	r, ok, err := seq.ReduceeOK(s, func(i1, i2 int) (int, error) {
+		if i2 == 11 {
+			return i1, errors.New("stop")
+		}
+		return i1 + i2, nil
+	})
+	assert.True(t, ok)
+	assert.Equal(t, 1+3+5+7+9, r)
+	assert.ErrorContains(t, err, "stop")
+}
+
+func Test_ReduceeSumFirstErr(t *testing.T) {
+	s := seq.Of(1, 3, 5, 7, 9, 11)
+	r, ok, err := seq.ReduceeOK(s, func(_, _ int) (int, error) {
+		return 0, errors.New("stop")
+	})
+	assert.True(t, ok)
+	assert.Equal(t, 0, r)
+	assert.ErrorContains(t, err, "stop")
+}
+
+func Test_ReduceEmpty(t *testing.T) {
+	s := seq.Of[int]()
+	sum, ok := seq.ReduceOK(s, op.Sum)
+
+	assert.False(t, ok)
+	assert.Equal(t, 0, sum)
+}
+
+func Test_ReduceNil(t *testing.T) {
+	var s iter.Seq[int]
+	sum, ok := seq.ReduceOK(s, op.Sum)
+
+	assert.False(t, ok)
+	assert.Equal(t, 0, sum)
+}
+
+func Test_Contains(t *testing.T) {
 
 	ok := seq.Contains(seq.Of(1, 2, 3, 4, 5, 6), 5)
 
 	assert.True(t, ok)
 }
 
-func Test_ReduceFirst(t *testing.T) {
+func Test_First(t *testing.T) {
 
 	result, ok := seq.First(seq.Of(1, 2, 3, 4, 5, 6), more.Than(5)) //7, true
 
 	assert.True(t, ok)
 	assert.Equal(t, 6, result)
 
+}
+
+var even = func(v int) bool { return v%2 == 0 }
+
+func Test_Flat(t *testing.T) {
+	md := seq.Of([][]int{{1, 2, 3}, {4}, {5, 6}}...)
+	f := seq.Flat(md, func(i []int) []int { return i })
+	e := []int{1, 2, 3, 4, 5, 6}
+	assert.Equal(t, e, seq.Slice(f))
+}
+
+func Test_Filter(t *testing.T) {
+	s := seq.Of(1, 3, 4, 5, 7, 8, 9, 11)
+	r := seq.Filter(s, even)
+	assert.Equal(t, slice.Of(4, 8), seq.Slice(r))
+}
+
+func Test_Filt(t *testing.T) {
+	s := seq.Of(1, 3, 4, 5, 7, 8, 9, 11)
+	l := seq.Filt(s, func(i int) (bool, error) { return even(i), op.IfElse(i > 7, errors.New("abort"), nil) })
+	r, err := seq2.Slice(l)
+	assert.Error(t, err)
+	assert.Equal(t, slice.Of(4, 8), r)
 }
