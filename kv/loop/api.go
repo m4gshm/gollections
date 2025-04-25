c6086e8819e65abc78b9a59458862e2d6734375a
Bulgakov Alexander <aleksandr.bulgakov@raiffeisen.ru>
2025-04-25 11:52:55 +0300

    deprecate loop API

--- kv/loop/api.go
+++ kv/loop/api.go
@@ -1,4 +1,5 @@
 // Package loop provides helpers for loop operation over key/value pairs and iterator implementations
+// Deprecated: use the seq, seq, seq2 packages API instead.
 package loop
 
 import (
